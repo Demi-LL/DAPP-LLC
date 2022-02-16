7c1c3531929f1ba4def86b7039fc70357220287f
Demi-LL <liuliunft@gmail.com>
2022-02-16 23:28:38 +0800

    feat: 調整大數的使用方式

--- front/libraries/transfer.js
+++ front/libraries/transfer.js
@@ -4,7 +4,7 @@ export default {
    * 大數需要另外使用套件處理，否則會出現溢位錯誤
    */
   decimalsToWei(num, decimals = 18) {
-    return new BigNumber(parseFloat(num) * 10 ** decimals);
+    return (new BigNumber(parseFloat(num))).multipliedBy((new BigNumber(10)).exponentiatedBy(decimals));
   },
   weiToDecimals(num, decimals = 18) {
     return parseFloat(num) / 10 ** decimals;
