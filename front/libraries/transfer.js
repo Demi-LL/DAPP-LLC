c23487199b50e3b0c84a0cdb783f8f0b09da285d
Demi-LL <liuliunft@gmail.com>
2022-02-17 00:26:10 +0800

    feat: 調整大數所使用套件
    1. MAC 使用 bignumber.js 開發成功，但在 linux 環境會出現錯誤，改用 web3-utils 後解決

--- front/libraries/transfer.js
+++ front/libraries/transfer.js
@@ -1,10 +1,12 @@
-import BigNumber from "bignumber.js";
+import Utils from 'web3-utils';
+
 export default {
   /**
    * 大數需要另外使用套件處理，否則會出現溢位錯誤
+   * 2022-02-17 使用 bignumber.js 套件，在本地 MAC 開發成功，但使用 Linux 會出現 invalid BigNumber value 錯誤
    */
-  decimalsToWei(num, decimals = 18) {
-    return (new BigNumber(parseFloat(num))).multipliedBy((new BigNumber(10)).exponentiatedBy(decimals));
+  decimalsToWei(num) {
+    return Utils.toWei(num);
   },
   weiToDecimals(num, decimals = 18) {
     return parseFloat(num) / 10 ** decimals;
